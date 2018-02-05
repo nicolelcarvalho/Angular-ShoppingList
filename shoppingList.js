6339b1d56e267c1d265bbb76266b34c8c26db926
Nicole Carvalho <NicoleCarvalho@NicoleCalhosMBP.home>
2018-02-05 15:53:19 -0500

    created simple shopping list app

--- shoppingList.js
+++ shoppingList.js
@@ -1,7 +1,10 @@
 var app = angular.module("shoppingList", []);
 // Set up the controller. Scope is the model that binds the data between the controller and view (html)
+
 app.controller("myCtrl", function($scope) {
+	
 	$scope.products = ["Milk", "Bread", "Cheese"];
+
 	// Add an item to the products array 
 	$scope.addItem = function() {
 		$scope.errortext = "";
@@ -16,8 +19,10 @@ app.controller("myCtrl", function($scope) {
 			$scope.errortext = "The item is already in your shopping list.";
 		}
 	}
+
 	// Remove an item from the products array 
 	$scope.removeItem = function(i) {
 		$scope.products.splice(i, 1);
 	}
+
 });
